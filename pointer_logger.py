da4a43bd4f303a7055194f60459029cd9f73d55f
Gou <runanashi@ymail.ne.jp>
2024-12-28 18:05:01 +0700

    Don't buffer print output

--- pointer_logger.py
+++ pointer_logger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S python3 -u
 '''This little utility will repeatedly read an emulated-system specific pointer defined by 2 bytes and print
 how did this pointer value change. This will give you an idea how the data is consumed by an unknown software.
 
